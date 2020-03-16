0e95d3f5c496532dc34392cb68c40ac305673c03
justin21453 <61999429+justin21453@users.noreply.github.com>
2020-03-16 21:34:01 +0800

    Add files via upload

---
+++ PairAndTuple.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <utility>
+#include <tuple>
+#include <string>
+#include <stdexcept>
+using namespace std;
+
+int main()
+{
+    pair<int, double> p(10, 20.5);
+    tuple<int, int, char, string> t(5, 12, 'A', "abc");
+
+    cout << p.first << " ";
+    cout << p.second << " ";
+    cout << endl;
+    cout << get<0>(t) << " ";
+    cout << get<1>(t) << " ";
+    cout << get<2>(t) << " ";
+    cout << get<3>(t) << " ";
+    cout << endl;
+
+    return 0;
+}
